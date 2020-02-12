b04ac7bb238582b1939e536202d7c8da623d58da
Anggito Wibisono Putro <anggito@kanosolution.com>
2020-02-12 11:57:56 +0700

    update giis and dashboard view

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -13,7 +13,33 @@ function setInfoWindowIndustri(marker, industri) {
                         <br>
                         ${industri.alamat}
                         <br>
-                        Telp: ${industri.no_telp}
+                        Telp: ${industri.no_telp}                        
+                    </td>
+                </tr>
+            </table>`
+            
+        }); 
+
+    marker.addListener('click', function() {infowindow.open(map, marker); });
+}
+
+
+function setInfoWindowIndustri(marker, industri) {
+    var infowindow = new google.maps.InfoWindow({
+            content: `
+            <table>
+                <tr>
+                    <td> 
+                        <img src='${industri.foto_gedung}' width='50' height='50'/>
+                    </td>
+                    <td class='pl-2'>
+                        <label>
+                            ${industri.nama}
+                        </label>
+                        <br>
+                        ${industri.alamat}
+                        <br>
+                        Telp: ${industri.no_telp}                        
                     </td>
                 </tr>
             </table>`
